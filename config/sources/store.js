5d17099579ff233827c8dea695328b15adf06a19
Kjell-Morten <post@kjellmorten.no>
2017-09-25 14:00:35 +0200

    Use json adapter instead of couchdb adapter

--- config/sources/store.js
+++ config/sources/store.js
@@ -1,13 +1,15 @@
 module.exports = {
   id: 'store',
-  adapter: 'couchdb',
+  adapter: 'json',
   auth: 'couchdb',
   baseUri: `${process.env.GR8_COUCHDB_URL}/${process.env.GR8_COUCHDB_DB}`,
+  handleMeta: true,
   endpoints: {
-    get: {uri: '/_all_docs', path: 'rows[]'},
+    get: {uri: '/_design/store/_view/by_type_updatedAt?include_docs=true&startkey=["{type}"{updatedAfter|wrap(\\,",")?}]&endkey=["{type}",\\{\\}]', path: 'rows[].doc'},
     getOne: '/{type}:{id}',
     set: {uri: '/_bulk_docs', path: 'docs[]', method: 'POST'},
     setOne: '/{type}:{id}',
+    getRevs: {uri: '/_all_docs{?keys=ids|wrap([, ", ", ]),include_docs=includeDocs?}', path: 'rows[]'},
     getMeta: '/{type}:{id}',
     setMeta: '/{type}:{id}'
   },
@@ -15,5 +17,7 @@ module.exports = {
     '*': {
       transform: ['removeTypePrefixOnId']
     }
-  }
+  },
+  beforeSerialize: 'couchdb-beforeSerialize',
+  afterNormalize: 'couchdb-afterNormalize'
 }
